d91662f946b4f405deff43dc3b677bd359f1961a
Volkan Altundal <volkan@Volkans-MBP.fritz.box>
2019-01-17 19:39:36 +0100

    init

---
+++ src/app/services/scrape.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import * as pup from "puppeteer";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ScrapeService {
+
+  public async getQuokaList() {
+    const browser = await pup.launch()
+    const page = await browser.newPage()
+    await page.goto('https://www.quoka.de/')
+
+    await page.evaluate(() => {
+      const searchDescription = document.querySelector('#search1') as HTMLInputElement
+      searchDescription.value = '3 Zimmerwohnung'
+
+      const searchPLZ = document.querySelector('#city') as HTMLInputElement
+      searchPLZ.value = 'Coburg'
+
+      const searchButton = document.querySelector('#searchbutton') as HTMLButtonElement
+      searchButton.click()
+    })
+
+    await page.waitForNavigation()
+
+    await page.evaluate(() =>
+      Array.from(document.querySelectorAll('div.q-col div.description'))
+        .map(x => x.innerHTML.trim()))
+
+    await browser.close()
+  }
+}

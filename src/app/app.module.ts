f6861f0e02f7087ffc914b19835cba8cd9ce3cf0
Volkan Altundal <volkan@Volkans-MBP.fritz.box>
2019-01-18 11:26:51 +0100

    added house service and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HouseListComponent } from './components/house-list/house-list.component';
@@ -10,7 +11,8 @@ import { HouseListComponent } from './components/house-list/house-list.component
     HouseListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

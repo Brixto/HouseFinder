2dc7b56466c6497ec9bb2477e7b7a272dfc0fa16
Volkan Altundal <volkan@Volkans-MBP.fritz.box>
2019-01-18 10:10:29 +0100

    scrape service logic removed

--- src/app/services/scrape.service.ts
+++ src/app/services/scrape.service.ts
@@ -6,28 +6,5 @@ import * as pup from "puppeteer";
 })
 export class ScrapeService {
 
-  public async loadQuokaList() {
-    const browser = await pup.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.quoka.de/')
 
-    await page.evaluate(() => {
-      const searchDescription = document.querySelector('#search1') as HTMLInputElement
-      searchDescription.value = '3 Zimmerwohnung'
-
-      const searchPLZ = document.querySelector('#city') as HTMLInputElement
-      searchPLZ.value = 'Coburg'
-
-      const searchButton = document.querySelector('#searchbutton') as HTMLButtonElement
-      searchButton.click()
-    })
-
-    await page.waitForNavigation()
-
-    await page.evaluate(() =>
-      Array.from(document.querySelectorAll('div.q-col div.description'))
-        .map(x => x.innerHTML.trim()))
-
-    await browser.close()
-  }
 }
